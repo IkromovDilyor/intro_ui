7617a667940899390b7f67384612388de897fa6f
IkromovDilyor <dilyorbeki@mail.ru>
2021-05-20 13:51:37 +0500

    intro_ui

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,167 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:intro_ui/pages/home_page.dart';
 import 'package:intro_ui/utils/Strings.dart';
 
 import 'home_page.dart';
-
-
-class IntroPage extends StatefulWidget {
-  static final String id = "intro_page";
-
+class IntroPage extends StatefulWidget{
+  static final String id='intro_page';
   @override
-  _IntroPageState createState() => _IntroPageState();
+  _IntroPageState createState()=>_IntroPageState();
 }
-
-class _IntroPageState extends State<IntroPage> {
-
-  PageController _pageController;
-  int currentIndex = 0;
-
+class _IntroPageState extends State<IntroPage>{
+  PageController _controller;
+  int _pageIndex=0;
   @override
-  void initState() {
-    _pageController = PageController(
-        initialPage: 0
-    );
+  void initState(){
     super.initState();
+    _controller=PageController(
+      initialPage:0,
+    );
   }
-
   @override
-  void dispose() {
-    _pageController.dispose();
+  void dispose(){
     super.dispose();
+    _controller.dispose();
   }
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.white,
-
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
+      backgroundColor:Colors.white,
+      body:Stack(
+        alignment:Alignment.bottomCenter,
+        children:[
+          //pages
           PageView(
-            onPageChanged: (int page) {
-              setState(() {
-                currentIndex = page;
+            controller:_controller,
+            onPageChanged:(int index){
+              setState((){
+                _pageIndex=index;
               });
             },
-            controller: _pageController,
-            children: [
+            children:[
+              //first page
               makePage(
-
-                title: Strings.stepOneTitle,
-                content: Strings.stepOneContent,
-                image: 'assets/images/image_1.png',
-
-
+                title:Str.firstTitle,
+                content:Str.firstContent,
+                image:'assets/images/image_1.png',
               ),
+              //second page
               makePage(
-                  reverse: true,
-                title: Strings.stepTwoTitle,
-                content: Strings.stepTwoContent,
-
-
-                image: 'assets/images/image_2.png',
+                title:Str.secondTitle,
+                content:Str.secondContent,
+                image:'assets/images/image_2.png',
               ),
+              //third page
               makePage(
-
-                  title: Strings.stepThreeTitle,
-                  content: Strings.stepThreeContent,
-                image: 'assets/images/image_3.png',
-
+                title:Str.thirdTitle,
+                content:Str.thirdContent,
+                image:'assets/images/image_3.png',
               ),
-
             ],
           ),
           Container(
-            margin: EdgeInsets.only(bottom: 60),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: _buildIndicator(),
+            margin:EdgeInsets.only(bottom:50),
+            child:Row(
+              mainAxisAlignment:MainAxisAlignment.center,
+              children:<Widget>[...?_indicatorBuilder()],
             ),
-
           ),
-          Container(
-            margin: EdgeInsets.only(left: 280,bottom: 50),
-            child:   GestureDetector(
-              onTap: (){
-                Navigator.pushReplacementNamed(context, HomePage.id);
-              },
-              child: Padding(
-                padding: EdgeInsets.only(right: 20,left: 20),
-                child: Text('Skip',style: TextStyle(color: Colors.green,fontSize: 18,fontWeight: FontWeight.w400),),
-              ),
+          //#SKIP BUTTON
+          if(_pageIndex==2)Container(
+            margin:EdgeInsets.only(right:30,bottom:43),
+            child:Row(
+              mainAxisAlignment:MainAxisAlignment.end,
+              children:[
+                _skipButton(title:'Skip'),
+              ],
             ),
-          )
-
+          ),
         ],
-
       ),
-
     );
-
   }
-
-  Widget makePage({  title, content, image, text, reverse = false}) {
+  Widget makePage({title,content,image}){
+    //page elements
     return Container(
-      padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-
-          Column(
-            children: <Widget>[
-
-              SizedBox(height: 10,),
-            ],
+      padding:EdgeInsets.only(left:50,right:50,top:50),
+      child:Column(
+        mainAxisAlignment:MainAxisAlignment.center,
+        children:[
+          //first text
+          Text(
+            title,
+            textAlign:TextAlign.center,
+            style:TextStyle(fontSize:30,color:Colors.deepOrange,fontWeight:FontWeight.w700,),
           ),
-          Text(title, style: TextStyle(
-              color: Colors.green,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-          ),),
-          SizedBox(height: 30,),
-          Text(content, textAlign: TextAlign.center, style: TextStyle(
-              color: Colors.grey,
-              fontSize: 20,
-              fontWeight: FontWeight.w400
-          ),),
+          //second text
+          SizedBox(height:20),
+          Text(
+            content,
+            textAlign:TextAlign.center,
+            style:TextStyle(fontSize:20,color:Colors.black26,fontWeight:FontWeight.w500),
+          ),
+          //page image
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Image.asset(image),
+            padding:EdgeInsets.all(18),
+            child:Image.asset(image),
           ),
-
         ],
-
       ),
     );
   }
-
-  Widget _indicator(bool isActive) {
+  Widget _indicator(bool isActive){
     return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
-      height: 6,
-      width: isActive ? 30 : 6,
-      margin: EdgeInsets.only(right: 5),
-      decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(5)
+      duration:Duration(milliseconds:200),
+      height:6,
+      margin:EdgeInsets.only(right:5),
+      width:isActive ? 30:6,
+      decoration:BoxDecoration(
+        borderRadius:BorderRadius.circular(5),
+        color:Colors.deepOrange,
       ),
     );
   }
-
-  List<Widget> _buildIndicator() {
-    List<Widget> indicators = [];
-    for (int i = 0; i<3; i++) {
-      if (currentIndex == i) {
-        indicators.add(_indicator(true));
-      } else  {
-        indicators.add(_indicator(false));
+  List<Widget> _indicatorBuilder(){
+    List<Widget> indicator=new List();
+    for(int i=0;i<3;i++){
+      if(_pageIndex==i){
+        indicator.add(_indicator(true));
+      }else{
+        indicator.add(_indicator(false));
       }
     }
-    return indicators;
+    return indicator;
   }
-
+  //Widget type funk for create button
+  Widget _skipButton({title}){
+    return Container(
+      child:GestureDetector(
+        onTap:()async{
+          await Navigator.pushReplacementNamed(context,HomePage.id);
+        },
+        child:Text(title,style:TextStyle(color:Colors.deepOrange,fontSize:20),),
+      ),
+    );
+  }
 }
-
-
-
--- lib/utils/Strings.dart
+++ lib/utils/Strings.dart
@@ -1,10 +1,8 @@
-
-
-class Strings {
-  static var stepOneTitle = "Learn from experts";
-  static var stepOneContent = "Select from top instructors around the world";
-  static var stepTwoTitle = "Go at your own pace";
-  static var stepTwoContent = "Enjoy lifetime access to courses on Udemy’s website and app";
-  static var stepThreeTitle = "Find video courses";
-  static var stepThreeContent = "Build your library for your career and personal growth";
+class Str{
+  static String firstTitle='Learn from experts';
+  static String firstContent='Select from top instructors around the world';
+  static String secondTitle='Go at your own pace';
+  static String secondContent='Enjoy lifetime access to courses on Udemy’s website and app';
+  static String thirdTitle='Find video courses';
+  static String thirdContent='Build your library for your career and personal growth';
 }
